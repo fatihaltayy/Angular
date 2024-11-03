8b3ffe705256a32c19fc12708e445469a7cd7682
Fatih Altay <sayinaltay@gmail.com>
2024-11-03 23:49:23 +0300

    Add files via upload

---
+++ 2.Giris/my-first-app/src/app/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component(
+    {
+        selector : "app-about",
+        standalone : true,
+        templateUrl :"./about.component.html",
+        styleUrl : "./about.component.css",
+        imports:[],
+        providers : [],
+        encapsulation : ViewEncapsulation.None
+    }
+)
+export class AboutComponent{
+
+
+}
